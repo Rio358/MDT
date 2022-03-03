5e62a80b52d487179de1f50181482e5996c7d8f3
Beep Boop <joshuarmckeon@hotmail.com>
2022-03-03 00:35:26 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,21 +7,17 @@ version '1.0.0'
 
 lua54 'yes'
 
-
-server_script 'sv_main.lua'
+shared_script '@es_extended/imports.lua'
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'sv_main.lua'
+}
 client_script 'cl_main.lua'
 
 ui_page 'ui/dashboard.html'
 
 files {
-    'ui/img/sasp_badge.png',
-    'ui/img/ems_badge.png',
-    'ui/img/court.png',
-    'ui/img/warrant_pfp.png',
-    'ui/img/profile_pic.png',
-    'ui/img/not-found.jpg',
-    'ui/img/male.png',
-    'ui/img/female.png',
+    'ui/img/*.png',
 	'ui/img/cars/*.png',
     'ui/dashboard.html',
     'ui/dmv.html',
